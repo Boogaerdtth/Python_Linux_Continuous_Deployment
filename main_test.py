c93bfb7e58ea323e65c7cf550b27a70791fee5a1
root <jasper17@live.nl>
2021-02-19 12:25:18 +0000

    other tests

--- main_test.py
+++ main_test.py
@@ -4,7 +4,7 @@ from main import app
 def test_main():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b'Doe normaal!'
+    assert response.data == b'Doe eens normaal!'
 
     response = app.test_client().get('/cow')
     assert response.status_code == 200
