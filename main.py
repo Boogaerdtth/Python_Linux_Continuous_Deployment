393ea121124b1ce85ebf4933fe588f8f764b631f
root <jasper17@live.nl>
2021-02-19 12:16:48 +0000

    add main_test file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
     return 'Doe eens normaaaaaaaaal!'
---
+++ main_test.py
@@ -0,0 +1,10 @@
+from main import app
+
+
+def test_main():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, world!'
+    response = app.test_client().get('/cow')
+    assert response.status_code == 200
+    assert response.data == b'MOoooOo!'
